b2b362dee8cc976ca8d0c67fd63b7b6ef9d8dc7c
liyanbit <liyanbit@outlook.com>
2019-11-18 00:31:30 -0800

    process rock image

---
+++ opencv-cplusplus-sample-LY01/opencv-cplusplus-sample-LY01/opencv-cplusplus-sample-LY01.cpp
@@ -0,0 +1,47 @@
+// opencv-cplusplus-sample-LY01.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+#include "opencv2/core/core.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+
+using namespace cv;
+using namespace std;
+
+void showImage(string windowName, Mat image)
+{
+	namedWindow(windowName);
+	imshow(windowName, image);
+}
+
+int main(int argc, char** argv)
+{
+	if (argc > 1)
+	{
+		// Load original image
+		Mat originalImage = imread(argv[1], CV_LOAD_IMAGE_COLOR);
+		showImage("Rock 1 - Original Image", originalImage);
+
+		// Convert color image to grey-scale
+		Mat greyImage;
+		cvtColor(originalImage, greyImage, COLOR_BGR2GRAY);
+		showImage("Rock 2 - cvtColor(originalImage, greyImage, COLOR_BGR2GRAY)", greyImage);
+
+		// Filter by grey-scale depth
+		Mat thresholdImage;
+		threshold(greyImage, thresholdImage, 120, 255, THRESH_BINARY);
+		showImage("Rock 3 - threshold(greyImage, thresholdImage, 120, 255, THRESH_BINARY)", thresholdImage);
+
+		// Invert color
+		Mat invertedImage;
+		bitwise_not(thresholdImage, invertedImage);
+		showImage("Rock 4 - bitwise_not(thresholdImage, invertedImage);", invertedImage);
+
+		waitKey(0);
+	}
+
+	return 0;
+}
+
