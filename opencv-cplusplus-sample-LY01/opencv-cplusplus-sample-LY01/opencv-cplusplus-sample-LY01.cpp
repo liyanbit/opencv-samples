5826304332b3e2823ac7b7dd995d485c908e6097
Patrick <PatrickWangBit@outlook.com>
2019-11-21 21:08:19 +0800

    This comment line is added by PatrickWangBit from VS2015@Home

--- opencv-cplusplus-sample-LY01/opencv-cplusplus-sample-LY01/opencv-cplusplus-sample-LY01.cpp
+++ opencv-cplusplus-sample-LY01/opencv-cplusplus-sample-LY01/opencv-cplusplus-sample-LY01.cpp
@@ -81,7 +81,7 @@ int main(int argc, char** argv)
 
 	// This comment line is added by PatrickWangBit from VS2019@Home
 
-	// ???
+	// This comment line is added by PatrickWangBit from VS2015@Home
 
 	return 0;
 }
